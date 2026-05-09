518212142f24108466c40218f16b3beff9df1564
Test user <yourextended@test.com>
2026-05-09 23:56:15 +0530

    fix: X button closes menu, bridge gap between hamburger and dropdown
    - X click adds .is-closed to override CSS hover
    - Hover on hamburger removes .is-closed so hover works again
    - Button ::after pseudo-element bridges 10px gap to dropdown
    - Increased bottom padding on hamburger for smoother reach

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -1,11 +1,22 @@
 /*
-* Responsive Navigation Toggle
-*/
+ * Responsive Navigation Toggle
+ */
 
 var $btn = $('#site-nav button');
 var $vlinks = $('#site-nav .visible-links');
 
 $btn.on('click', function () {
   $vlinks.toggleClass('is-open');
+  $vlinks.removeClass('is-closed');
   $(this).toggleClass('close');
+
+  // When closing (X clicked), add is-closed to override CSS hover
+  if (!$vlinks.hasClass('is-open')) {
+    $vlinks.addClass('is-closed');
+  }
+});
+
+// Remove is-closed when hovering the button (so CSS hover works again)
+$btn.on('mouseenter', function () {
+  $vlinks.removeClass('is-closed');
 });
